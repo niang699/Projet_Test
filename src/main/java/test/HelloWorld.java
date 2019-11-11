39059a868328c10848f4195a8097c91c9a442be6
niang699 <asta2niang@gmail.com>
2019-11-11 12:50:10 +0100

    first commit

---
+++ src/main/java/test/HelloWorld.java
@@ -0,0 +1,18 @@
+package test;
+
+public class HelloWorld 
+{
+	public String getMessage( String chaine)
+	{
+		if(chaine==null)
+		return "Hello World";
+		else
+			return chaine;
+	}
+	
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+	
+}
---
+++ src/test/java/testhello/HelloWorldTest.java
@@ -0,0 +1,19 @@
+package testhello;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HelloWorldTest extends TestCase {
+
+	public void testGetMessage() {
+		
+	}
+	@Test
+	public void testSum() 
+	{
+		
+	}
+
+}
